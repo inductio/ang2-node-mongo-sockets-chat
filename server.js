ff7277a52d760223fe0e46da2c126518eeede0f5
Aleksandr Shustrov <aleksandr.shustrov@gmail.com>
2016-10-04 17:36:05 +0300

    project init

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var url = require('url');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://jsmp_chatdb:jsmp_chatdb@ds035016.mlab.com:35016/jsmp_chatdb");
+
+var Message = mongoose
+    .model("Message", new mongoose
+        .Schema({text: String})
+    );
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get("/api/messages", function (req, res) {
+    Message
+        .find({})
+        .lean()
+        .exec()
+        .then(function (messages) {
+            res.json(messages);
+        });
+});
+
 app.use('/public', express.static('public'));
+
+io.on('connection', function (socket) {
+    socket.on('message receive', function (msg) {
+        io.emit('message receive', msg);
+        if (msg) Message.create({text: msg});
+    });
+});
 
 http.listen(8080, function () {
     console.log("App started on localhost:8080");
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { NgModule, Component, Input, Attribute } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { ActivatedRoute, RouterModule } from '@angular/router';
@@ -9,6 +9,7 @@ import { default as Greeter } from './services';
   selector: 'hello',
   template: '<p>{{ message }}</p>',
 })
+
 export class Hello {
   constructor(greeter:Greeter) {
     this.message = greeter.say('hello', 'Angular 2');
@@ -16,43 +17,34 @@ export class Hello {
 }
 
 @Component({
-  selector: 'ciao',
+  selector: 'Bye',
   template: '<p>{{ message$ | async }}</p>',
 })
-export class Ciao {
+
+export class Bye {
   constructor(greeter:Greeter, route:ActivatedRoute) {
     this.message$ = route.params
-    .map(params => greeter.say('Bye', params.name));
+        .map(params => greeter.say('Bye', params.name));
   }
-}
-
-@Component({
-  selector: 'linker',
-  template: '<p>' +
-  '<a [href]="url" [title]="name">{{ name }}</a>' +
-  '</p>',
-})
-export class Linker {
-  @Input() url;
-  @Attribute('name') name;
 }
 
 @Component({
   selector: 'hello-app',
   template: `
   <ul>
-  <li><a [routerLink]="['/']">Hello</a></li>
-  <li><a [routerLink]="['/ciao', 'ng2']">Bye</a></li>
+    <li><a [routerLink]="['/']">Hello</a></li>
+    <li><a [routerLink]="['/bye', 'ng2']">Bye</a></li>
   </ul>
   <router-outlet></router-outlet>
   `,
 })
+
 export class HelloApp {
 }
 
 const routing = RouterModule.forRoot([
   { path: '', component: Hello },
-  { path: 'ciao/:name', component: Ciao },
+  { path: 'bye/:name', component: Bye },
 ]);
 
 @NgModule({
@@ -63,8 +55,7 @@ const routing = RouterModule.forRoot([
   declarations: [
     HelloApp,
     Hello,
-    Ciao,
-    Linker,
+    Bye,
   ],
   providers: [
     Greeter,
